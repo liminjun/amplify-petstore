c745dd0f60fb30aae4606ebf4c910f0a1cf8cad0
liminjun <v-leeli@hotmail.com>
2023-03-06 21:10:44 +0800

    feat:添加详情逻辑

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
 import logo from './logo.svg';
 import './App.css';
 import '@aws-amplify/ui-react/styles.css';
+import { useState } from 'react';
 
 import { NavBar, Pets, MarketingFooter, ProfileCard, PetDetail } from './ui-components';
+import { set } from 'lodash';
 
 function App() {
   const petProfileOverride = {
     Breed: {color:"blue"}
   }
+  const [showDetails, setShowDetials] = useState(false);
+  const [pet, setPet] = useState({});
+  const [updatePet, setUpdatePet] = useState({});
+  const [name, setName] = useState("");
+  const [color, setColor] = useState("");
+  const [age, setAge] = useState("");
+  const [breed, setBreed] = useState("");
+  const [about, setAbout] = useState("");
+  const [image, setImage] = useState("");
   return (
     <>
       <div className="App">
         <NavBar width={"100%"}></NavBar>
         <header>
-          <PetDetail/>
+          {
+            showDetails && <PetDetail pet={pet} overrides={{
+              MyIcon: {
+                onClick: () => {
+                  setShowDetials(false);
+                },
+                style: {
+                  cursor: "pointer"
+                }
+              }
+            }}/>
+          }
           <Pets overrideItems={ ({item,index})=>({
             overrides: {
               About: {color:"blue"},
               Button29766907: {
                 onClick: () => {
                   alert(`${item.name}`);
+                  setShowDetials(!showDetails);
+                  setPet(item);
+                }
+              },
+              Button36492693: {
+                onClick: () => {
+                  if (!showForm) {
+                    setShowForm(true);
+                    setUpdatePet(item);
+
+                    setName(item.name);
+                    setColor(item.color);
+                    setAge(item.age);
+                    setBreed(item.breed);
+                    setAbout(item.about);
+                    setImage(item.image);
+                  }
                 }
               }
             }
